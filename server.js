4e462191f6052ee47d5795c24b01da680471c905
jloures <john_loures@hotmail.com>
2016-10-11 15:43:00 -0400

    First commit:

---
+++ server.js
@@ -0,0 +1,45 @@
+// server.js
+
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = 3000;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.post('/login', function(req, res) {
+    res.json(req.body);   
+});
+
+router.post('/signup', function(req, res) {
+    res.json(req.body);   
+});
+
+router.post('/recovery', function(req, res) {
+    res.json(req.body);   
+});
+
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
