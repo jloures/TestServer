d8a4fb1a70da8ffad74edcceb1d7ec7da4105fbd
jloures <john_loures@hotmail.com>
2016-10-12 01:42:08 -0400

    Can create users in memory

--- server.js
+++ server.js
@@ -3,6 +3,9 @@
 // BASE SETUP
 // =============================================================================
 
+//these are the globals for our server (everything will be in memory)
+var users = [];
+
 // call the packages we need
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
@@ -21,11 +24,21 @@ var router = express.Router();              // get an instance of the express Ro
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.post('/login', function(req, res) {
-    res.json(req.body);   
+    var index = findUser(req.body.username)
+    if( index > -1 ) {
+        res.json(req.body);
+    } else {
+        res.status(404).send('No User found!');
+    }  
 });
 
 router.post('/signup', function(req, res) {
-    res.json(req.body);   
+    if( (findUser(req.body.username) === -1) && (findEmail(req.body.email) === -1)) {
+        users.push(req.body);
+        res.json(req.body);
+    } else {
+        res.status(409).send('Conflict!');
+    }  
 });
 
 router.post('/recovery', function(req, res) {
@@ -43,3 +56,25 @@ app.use('', router);
 // =============================================================================
 app.listen(port);
 console.log('Magic happens on port ' + port);
+
+var findUser = function(username) {
+    var index = -1;
+    for(var i = 0; i < users.length; i++) {
+        if(users[i].username === username ) {
+            index = i;
+            break;
+        }
+    }
+    return index;
+}
+
+var findEmail = function(email) {
+    var index = -1;
+    for(var i = 0; i < users.length; i++) {
+        if(users[i].email === email ) {
+            index = i;
+            break;
+        }
+    }
+    return index;
+}
