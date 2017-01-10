2cf4a7cb2a3d07bedb1051d355dad7a615d43c7c
jloures <john_loures@hotmail.com>
2017-01-10 18:57:47 -0500

    Added route for matches that will be used from milestone6 ownwards

--- server.js
+++ server.js
@@ -192,6 +192,11 @@ router.post('/:userId/creategame', function(req, res) {
     res.json({id: game.id});   
 });
 
+router.post('/:userId/:gameId/match', function(req, res) {
+    console.log(req)
+    res.status(200).send('Ok');  
+});
+
 router.post('/:userId/duplicategame', function(req, res) {
     var gameDuplicate = findGame(req.body.id);
     if( gameDuplicate == null ) {
